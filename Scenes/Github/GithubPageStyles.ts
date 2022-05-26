37d4163aca7f1e710b1e1966b617b6eeca4e9343
cd-user <cd-user@my-app.com>
2022-05-26 19:49:47 +0000

    Updates

--- Scenes/Github/GithubPageStyles.ts
+++
@@ -1,8 +0,0 @@
-import { css, SerializedStyles } from '@emotion/react';
-
-export const getGithubHeaderStyles = (): SerializedStyles =>
-  css({
-    backgroundImage: 'url("/assets/github-background-black-.jpg")',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-  });
