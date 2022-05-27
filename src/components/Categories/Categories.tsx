d9287749eb5fe9a213b9a14c88eaff73542ee86f
Andrés Rojas <eurekacloud2019@gmail.com>
2022-05-27 14:50:27 +0200

    Fix categories url clicks

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import axios from 'axios';
 import { getCategoriesStyles, getSectionStyles } from './CategoriesStyles';
 import { categories } from '../../api/Data/categories';
+import { useNavigate } from 'react-router-dom';
 // import { useNavigate } from 'react-router-dom';
 
+interface Categories {
+  name: string;
+  url: string;
+  backgroundImage: string;
+  backgroundImageHover: string;
+}
+
 export const Categories: React.FC = () => {
-  interface Categories {
-    name: string;
-    url: string;
-    backgroundImage: string;
-    backgroundImageHover: string;
-  }
+  const navigate = useNavigate();
 
   // const [categories, setCategories] = React.useState<Categories[]>([]);
   // const navigate = useNavigate();
@@ -38,14 +41,14 @@ export const Categories: React.FC = () => {
   return (
     <section css={getSectionStyles}>
       {categories.map((category, index) => (
-        <a
-          href={category.url}
+        <div
+          onClick={() => navigate(`${category.url}`)}
           key={index}
           id={category.name}
           css={getCategoriesStyles(category.name)}
         >
           <h3>{category.name}</h3>
-        </a>
+        </div>
       ))}
     </section>
   );
