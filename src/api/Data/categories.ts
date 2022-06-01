748d0f029b0448e9910716efb456ab19bc078eee
Andrés Rojas <eurekacloud2019@gmail.com>
2022-06-01 10:51:50 +0200

    Remove useless code

--- src/api/Data/categories.ts
+++ src/api/Data/categories.ts
@@ -2,25 +2,17 @@ export const categories = [
   {
     name: 'Github',
     url: '/Github',
-    backgroundbackgroundImage: '',
-    backgroundImageHover: '',
   },
   {
     name: 'Design',
     url: '/Design',
-    backgroundImage: '',
-    backgroundImageHover: '',
   },
   {
     name: 'Wordpress',
     url: '/Wordpress',
-    backgroundImage: '',
-    backgroundImageHover: '',
   },
   {
     name: 'Video',
     url: '/Video',
-    backgroundImage: '',
-    backgroundImageHover: '',
   },
 ];
