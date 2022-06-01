fb7ebb2f8c8f52ce87fc28617fc1fd961f3fe704
Andrés Rojas <eurekacloud2019@gmail.com>
2022-06-01 09:33:31 +0200

    Give margin to navbar logo

--- src/components/NavBar/NavBarStyles.ts
+++ src/components/NavBar/NavBarStyles.ts
@@ -3,6 +3,7 @@ import { css, SerializedStyles } from '@emotion/react';
 export const getHeaderLogoStyles = (): SerializedStyles =>
   css({
     maxWidth: '50px',
+    paddingRight: '1em',
   });
 
 export const getNavBarStyles = (): SerializedStyles =>
